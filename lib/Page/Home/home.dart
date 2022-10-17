68ce67af3842a69cfcca9bf9fc836457e12b6ff8
hjarroneDev <hjarrone.dev@gmail.com>
2022-10-17 18:21:27 +0200

    New Update

--- lib/Page/Home/home.dart
+++ lib/Page/Home/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:swipe_widget/swipe_widget.dart';
 import '../User/usuarios.dart';
-import 'Widget/carta_frente.dart';
-import 'Widget/carta_usada.dart';
-import 'Widget/carta_verso.dart';
-import 'Widget/cartas.dart';
+import 'Widget/princimpal/carta_usada.dart';
+import 'Widget/princimpal/carta_verso.dart';
+import 'Widget/princimpal/cartas.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -14,6 +12,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<int> naoUsada = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20
+  ];
+  List<int> usadas = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+  List<int> minhas = [31, 32, 33, 34, 35, 36];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -21,154 +44,47 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Container(
           color: Colors.grey[100],
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               const Usuarios(),
-              const SizedBox(
-                height: 10,
-              ),
+
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Stack(
-                    children: const [
-                      CartaVerso(),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 1),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 2),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 3),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 4),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 5),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 6),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 7),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 8),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 9),
-                        child: CartaVerso(),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 10),
-                        child: CartaVerso(),
-                      ),
-                    ],
+                  SizedBox(
+                    height: 300,
+                    width: 150,
+                    child: ListView.builder(
+                        itemCount: naoUsada.length,
+                        itemBuilder: ((context, index) {
+                          return const Align(
+                            alignment: Alignment.topCenter,
+                            heightFactor: 0.003,
+                            child: CartaVerso(),
+                          );
+                        })),
                   ),
-                  Stack(
-                    children: [
-                      CartaUsada(
-                        value: 'A',
-                        icon: Icons.heart_broken_rounded,
-                        color: Colors.red.shade300,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 1),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 2),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 3),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 4),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 5),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 6),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 7),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 8),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 9),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 1.6 * 10),
-                        child: CartaUsada(
-                          value: 'A',
-                          icon: Icons.heart_broken_rounded,
-                          color: Colors.red.shade300,
-                        ),
-                      ),
-                    ],
+                  SizedBox(
+                    height: 300,
+                    width: 150,
+                    child: ListView.builder(
+                        itemCount: usadas.length,
+                        itemBuilder: ((context, index) {
+                          return Align(
+                            alignment: Alignment.topCenter,
+                            heightFactor: 0.003,
+                            child: CartaUsada(
+                              value: 'A',
+                              icon: Icons.heart_broken_rounded,
+                              color: Colors.red.shade300,
+                            ),
+                          );
+                        })),
                   ),
                 ],
               ),
-              const Spacer(),
 
               //Text('$width'),
               const Cartas()
